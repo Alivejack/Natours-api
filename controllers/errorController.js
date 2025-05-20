a6844917ce3bcc5c4f6e3ad7c1db8e9ce1cfbcc1
Younes <yoones13844@gmail.com>
2025-05-20 18:52:32 +0330

    refactor: enhance error handling in production mode to include operational error checks

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,10 +8,24 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programing or other unkown error: don't leak error details
+  } else {
+    // 1) log error
+    console.error('ERROR 💥', err);
+
+    // 2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong !',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
