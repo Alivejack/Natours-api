693374fcd32429ea6ae0566c423b55a6fd65244a
Younes <yoones13844@gmail.com>
2025-05-25 12:33:36 +0330

    add login route and implement simple login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const appError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  // this is the old and nonsecure version of getting data from user
+  // this is the nonsecure version of getting data from user
   // const newUser = await User.create(req.body);
 
   // this is the secure way of getting data from user
@@ -26,3 +27,22 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) check if email and password exist
+
+  if (!email || !password) {
+    return next(new appError('Please provide email and password !'));
+  }
+
+  // 2) check if user exist && password is correct
+
+  // 3) if everything ok, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
