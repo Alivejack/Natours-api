2126a315b3afa903d0294b3f1c09476bd9ef518d
Younes <yoones13844@gmail.com>
2025-05-25 10:05:18 +0330

    refactor signup to use secure user data handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,16 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // this is the old and nonsecure version of getting data from user
+  // const newUser = await User.create(req.body);
+
+  // this is the secure way of getting data from user
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
