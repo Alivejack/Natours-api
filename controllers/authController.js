f2b67bde1b4f0d0d390572915a4bc6a64f5704fb
Younes <yoones13844@gmail.com>
2025-07-17 19:25:15 +0330

    made the passwordChangedAt field to work

--- controllers/authController.js
+++ controllers/authController.js
@@ -177,13 +177,13 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   if (!user) {
     return next(new AppError('Token is Invalid or has expired', 400));
   }
-  // 3) Update changedPasswordAt property for the user
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
   user.passwordResetToken = undefined;
   user.passwordResetExpires = undefined;
   await user.save();
 
+  // 3) Update changedPasswordAt property for the user
   // 4) Log the user in, send JWT
   const token = signToken(user._id);
 
