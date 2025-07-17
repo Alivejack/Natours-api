616903af3aaf604e46bb5731e15ef673979d0d3e
Younes <yoones13844@gmail.com>
2025-07-17 15:53:26 +0330

    implemented the resetPassword functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
@@ -138,7 +139,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `if you forgot your password submit a PATC request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    const emailSent = await sendEmail({
+    await sendEmail({
       to: user.email,
       subject: 'Your password reset token (valid only for 10 min)',
       text: message,
@@ -161,4 +162,33 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-// exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+  // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is Invalid or has expired', 400));
+  }
+  // 3) Update changedPasswordAt property for the user
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 4) Log the user in, send JWT
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
