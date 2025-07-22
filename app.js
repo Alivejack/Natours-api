1b2218ced3d44cf2c64318bddc90a27607ff3475
Younes <yoones13844@gmail.com>
2025-07-22 14:28:16 +0330

    limited the amount of requests per ip to 5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,10 +9,19 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: 'too many login attempts, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 // app.use(express.static(`${__dirname}/dev-data`));
