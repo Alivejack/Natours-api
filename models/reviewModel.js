49951f71e2704267c11997272b2fe950e74b73b3
Younes <yoones13844@gmail.com>
2025-07-27 14:50:46 -0600

    Implemented review Model

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      required: [true, 'A Review must have a rating from 1 to 5'],
+      min: [1, 'A Review rating minimum number is 1'],
+      max: [5, 'A Review rating maximum number is 1'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A Review must belong to a user'],
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'A Review must belong to a tour.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
